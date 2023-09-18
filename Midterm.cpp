557529bd16e6277e0c9a9b19a40ee40fdf4f29a3
HutsonHorton <142115137+HutsonHorton@users.noreply.github.com>
2023-09-18 12:05:24 -0700

    Start of Project; Created Animal Class including all of the organizable features

---
+++ Midterm.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+//Create Animal Class with Organizable Features
+class Animal{
+public:
+
+    int Age;
+    string Sex;
+    string Color;
+    int Weight;
+
+
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
